0495670a5658984c9eafc0eda1c841fdc05e7e10
vrsunilkumar <vrsunilkumar@gmail.com>
2022-04-20 19:58:02 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const express = require('express')
+const process = require('process')
+const geocode = require('../../weather-app/geocode')
+const forecast = require('../../weather-app/forecast')
+const exp = require('constants')
+
+console.log(__dirname)
+console.log(__filename)
+console.log(path.join(__dirname,'../public'))
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+const app = express()
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.send('<h1>Home Page</h1>')
+})
+
+app.get('/weather',(req,res)=>{
+    console.log('in weather')
+    console.log('location: ',req.query.location)
+    geocode(req.query.location,(error,{latitude,longitude,location}={}) => {
+        console.log('Error',error)
+        console.log('Data',latitude+longitude+location)
+        if (error) {
+            return console.log(error)
+        } else {
+            forecast(latitude, longitude, (error,frcstdata) => {
+                if (error) {
+                    return console.log(error)
+                } else {
+                    console.log('Location', location)
+                    console.log('Forecast', frcstdata)
+                    res.send({
+                        location: location,
+                        forecast: frcstdata
+                    })
+                }
+            })    
+        }
+    })
+})
+
+app.get('/products',(req,res)=>{
+    console.log(req.query)
+    if (!req.query.search) {
+        return res.send('Please provide a search string')
+    }
+    res.send({products: []})
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up and running on port 3000.')
+})
